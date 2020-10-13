d1e5bcbafacda1d8a31301dcaa053e019cb4dfb6
Công Sơn <71420045+congsonzero@users.noreply.github.com>
2020-10-13 19:29:19 +0700

    -Test

---
+++ Test.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main() {
+   // printf() displays the string inside quotation
+   float a=5.0,b=2.0,i=0;
+   printf("Gia tri a=%d\n",a);
+   printf("Gia tri b=%d",b);
+   if (i==0)
+   {printf("\ni is True");};
+   printf("\nket qua a/b %2f ",a/b);
+   return 0;
+}
