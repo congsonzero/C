79bb7567954f9e8157806aafa0761994e194d8fe
Công Sơn <71420045+congsonzero@users.noreply.github.com>
2020-10-15 19:41:22 +0700

    -Test

--- scanf.c
+++ scanf.c
@@ -1,15 +1,16 @@
-#include<stdio.h>
+#include <stdio.h>
 
 int main()
 {
     int a, b;
-
     printf("vui long nhap a: ");
     scanf("%d", &a);
-
     printf("vui long nhap b: ");
     scanf("%d", &b);
-
     printf("\nbien a=%d, bien b=%d", a, b);
+    int c, d;
+    printf("\nvui long nhap c,d: ");
+    scanf("%d%d",&c,&d);
+    printf("\nbien c=%d, bien d=%d", c, d);
     return 0;
 }
