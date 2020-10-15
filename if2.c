72003ccf823a44ec65b9fe3573075e5d8d10a05f
Công Sơn <71420045+congsonzero@users.noreply.github.com>
2020-10-15 20:38:52 +0700

    -Test

---
+++ if2.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main()
+{
+    int num1, num2;
+    printf("Nhap so thu nhat: ");
+    scanf("%d", &num1);
+    printf("Nhap so thu hai: ");
+    scanf("%d", &num2);
+    if (num1 > num2)
+    {
+        printf("So thu 1 lon hon so thu 2");
+    }
+    if (num1 == num2)
+    {
+        printf("hai so bang nhau");
+    }
+    else
+    {
+        printf("So thu 2 lon hon so thu 1");
+    }
+
+    return 0;
+}
