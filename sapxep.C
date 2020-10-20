b97a7fca5510c32c054824fcd44bdf017a4b1984
Công Sơn <71420045+congsonzero@users.noreply.github.com>
2020-10-20 20:31:09 +0700

    Test

--- sapxep.C
+++ sapxep.C
@@ -1,32 +1,36 @@
 #include <stdio.h>
 int main()
 {
-  int a, b, c, tam;
-  printf("a= ");
-  scanf("%d", &a);
-  printf("b= ");
-  scanf("%d", &b);
-  printf("c= ");
-  scanf("%d", &c);
-  if (a>b)
-  {
-      tam=a;
-      a=b;
-      b=tam;
-  }
-  if (a>c)
-  {
-      tam=a;
-      a=c;
-      c=tam;     
-  }
-  if(b>c)
-  {
-      tam=b;
-      b=c;
-      c=tam;
-  }
+    int a, b, c, tam;
+    printf("a= ");
+    scanf("%d", &a);
+    printf("b= ");
+    scanf("%d", &b);
+    printf("c= ");
+    scanf("%d", &c);
+    if (a > b)
+    {
+        //     tam = a;
+        //     a = b;
+        //     b = tam;
+        a = a + b - (b = a);
+    }
+    if (a > c)
+    {
+            tam = a;
+            a = c;
+            c = tam;
+    }
+    if (b > c)
+    {
+        //     tam = b;
+        //     b = c;
+        //     c = tam;
+        b = b + c;
+        c = b - c;
+        b = a - c;
+    }
+    printf("%d%d%d", a, b, c);
 
-  printf("%d%d%d",a,b,c);
-  return 0;
+    return 0;
 }
